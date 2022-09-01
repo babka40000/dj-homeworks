b79d097fed182157f74a715d9434dce1fb59e4e4
babka40000 <it-master@list.ru>
2022-09-01 10:28:29 +0400

    init

--- 3.2-crud/stocks_products/logistic/admin.py
+++ 3.2-crud/stocks_products/logistic/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from logistic.models import Product, StockProduct, Stock
 
-# Register your models here.
+
+class StockProductInline(admin.TabularInline):
+    model = StockProduct
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    inlines = [StockProductInline]
